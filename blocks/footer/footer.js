a26aac4bf107b128d573e2ee9239be00f8cb8dcc
Anjali-Kummari <anjali.kummari@valuemomentum.com>
2026-05-12 16:25:23 +0000

    Update footer links for insurance products

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -33,9 +33,9 @@ export default async function decorate(block) {
   insuranceCol.innerHTML = `
     <h4>Insurance</h4>
     <ul>
-      <li><a href="/products/homeowners">Homeowners</a></li>
-      <li><a href="/products/auto">Auto Insurance</a></li>
-      <li><a href="/products/business">Business Owners</a></li>
+      <li><a href="/homeowners">Homeowners</a></li>
+      <li><a href="/auto">Auto Insurance</a></li>
+      <li><a href="/business">Business Owners</a></li>
       <li><a href="/resources">Resource Center</a></li>
     </ul>`;
 
